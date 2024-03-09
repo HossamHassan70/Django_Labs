6cc8d8b8224453e82dcee4b51bb34ec4718b0c6b
Hossam Hassan <99551852+HossamHassan70@users.noreply.github.com>
2024-03-09 22:10:48 +0200

    Upload files For Lab 5

---
+++ company/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from django import forms
+from company.models import Employee
+from company.models import Employee, Team
+
+
+class EmployeeForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    salary = forms.IntegerField()
+    title = forms.CharField(max_length=50)
+    team = forms.ChoiceField(
+        choices=[(team.pk, team.pk) for team in Team.objects.all()], required=False
+    )
+
+
+class EmployeeForm2(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = "__all__"
